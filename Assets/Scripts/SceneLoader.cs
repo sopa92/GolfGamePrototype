6471f44addc3df33885ec67ca75e9169cef65750
Sofia Papadopoulou <sopa92@hotmail.com>
2020-02-10 11:34:27 +0000

    Changed the comment to something more clear

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,11 +2,7 @@
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
-{
-    /// <summary>
-    /// This function loads the scene with the given name
-    /// </summary>
-    /// <param name="sceneName"></param>
+{    
     public void LoadScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
