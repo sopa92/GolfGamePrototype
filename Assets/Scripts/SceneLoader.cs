288017da8d3466b14a21af1bbce7a976355ede55
Sofia Papadopoulou <sopa92@hotmail.com>
2020-02-10 09:33:32 +0000

    Updated the SceneLoader function + added some comments

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -2,7 +2,11 @@
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
-{    
+{
+    /// <summary>
+    /// This function loads the scene with the given name
+    /// </summary>
+    /// <param name="sceneName"></param>
     public void LoadScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
     }
